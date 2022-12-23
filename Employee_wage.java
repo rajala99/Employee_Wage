92ddbb89e0921a4b87802467cd0ac0b7a309282b
rajala99 <rajala.harini99@gmail.com>
2022-12-23 22:39:35 +0530

    ADDED: code for employee parttime wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,15 +1,20 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println(" Welcome to Employee wage computation program ");
-        final int Wage_per_hour = 20;
+        final int Wage_per_hours = 20;
+        final int part_time_hours = 4;
         final int fullday_hours = 8;
-        double employee = Math.floor(Math.random() * 10) % 2;
+        int pay = 0;
+        double employee = Math.floor(Math.random() * 10) % 3;
         if (employee == 1) {
-            System.out.println(" Employee is present ");
-           int pay = Wage_per_hour * fullday_hours;
-            System.out.println("Employee wage per day is : " + pay);
+            System.out.println(" Employee is full time");
+            pay = Wage_per_hours * fullday_hours;
+        } else if (employee == 2) {
+            System.out.println(" Employee is part time");
+            pay = Wage_per_hours * part_time_hours ;
         } else {
             System.out.println(" Employee is absent ");
         }
+        System.out.println("Employee daily wage is :" +pay);
     }
 }
