331d60f577db8898566bfa533391bcf55a252d71
rajala99 <rajala.harini99@gmail.com>
2022-12-23 22:35:08 +0530

    ADDED: code for employee fulltime wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,9 +1,13 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println(" Welcome to Employee wage computation program ");
+        final int Wage_per_hour = 20;
+        final int fullday_hours = 8;
         double employee = Math.floor(Math.random() * 10) % 2;
         if (employee == 1) {
             System.out.println(" Employee is present ");
+           int pay = Wage_per_hour * fullday_hours;
+            System.out.println("Employee wage per day is : " + pay);
         } else {
             System.out.println(" Employee is absent ");
         }
