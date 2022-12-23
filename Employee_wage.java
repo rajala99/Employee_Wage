cace3415023fa027c4de52921dee09e6b284551b
rajala99 <rajala.harini99@gmail.com>
2022-12-23 22:25:48 +0530

    ADDED: code for employee present or absent

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,5 +1,11 @@
 public class Employee_wage {
     public static void main(String[] args) {
-        System.out.println( " Welcome to Employee wage computation program ");
+        System.out.println(" Welcome to Employee wage computation program ");
+        double employee = Math.floor(Math.random() * 10) % 2;
+        if (employee == 1) {
+            System.out.println(" Employee is present ");
+        } else {
+            System.out.println(" Employee is absent ");
+        }
     }
 }
